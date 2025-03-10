54cd8740aaa1156227b10b1888ae0a0f8aae1b23
qwerrewq123 <preejer8013@gmail.com>
2025-03-10 14:50:07 +0900

    first commit

---
+++ src/main/java/com/tripplannerai/TripplannerAiApplication.java
@@ -0,0 +1,13 @@
+package com.tripplannerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripplannerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripplannerAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tripplannerai/TripplannerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripplannerai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripplannerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
