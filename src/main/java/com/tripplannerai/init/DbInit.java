64678a4e642c17116ba2c1de9bc573f0372c0efb
김민준 <kmj0376@gmail.com>
2025-03-19 17:51:20 +0900

    refactor: postconstruct,transactional 메서드 분리

--- src/main/java/com/tripplannerai/init/DbInit.java
+++ src/main/java/com/tripplannerai/init/DbInit.java
@@ -5,23 +5,35 @@ import com.tripplannerai.service.address.AddressService;
 import com.tripplannerai.service.category.CategoryService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DbInit {
     private final AddressService addressService;
     private final CategoryService categoryService;
 
+
     @PostConstruct
-    @Transactional
-    public void dbInit() {
+    public void init() {
         try {
-            addressService.saveAddressFromApi();
-            categoryService.saveCategoryFromApi();
+            initializeData();
         } catch (Exception e) {
-            throw new RuntimeException("데이터베이스 초기화 실패",e);
+            log.error("데이터베이스 초기화 실패: {}", e.getMessage(), e);
         }
+    }
+
+    @Transactional
+    public void initializeData() throws Exception {
+        log.info("주소 데이터 저장 시작...");
+        addressService.saveAddressFromApi();
+        log.info("주소 데이터 저장 완료");
+
+        log.info("카테고리 데이터 저장 시작...");
+        categoryService.saveCategoryFromApi();
+        log.info("카테고리 데이터 저장 완료");
     }
 }
