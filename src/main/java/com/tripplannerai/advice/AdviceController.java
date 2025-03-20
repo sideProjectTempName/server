baeb8cc953ce5e0fc671f383775a73a19cf509e0
preej <preejer8013@gmail.com>
2025-03-20 19:38:20 +0900

    advice order mapping

--- src/main/java/com/tripplannerai/advice/AdviceController.java
+++ src/main/java/com/tripplannerai/advice/AdviceController.java
@@ -16,26 +16,7 @@ import static com.tripplannerai.util.ConstClass.*;
 @Order(2)
 @RestControllerAdvice
 public class AdviceController {
-    @ExceptionHandler(NotFoundMemberException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundMemberException() {
-        return new ResponseEntity<>(ErrorResponse.of(NOT_FOUND_MEMBER_CODE,NOT_FOUND_MEMBER_MESSAGE), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(UnCorrectPasswordException.class)
-    public ResponseEntity<ErrorResponse> handleUnCorrectPasswordException() {
-        return new ResponseEntity<>(ErrorResponse.of(UN_CORRECT_PASSWORD_CODE,UN_CORRECT_PASSWORD_MESSAGE), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(NotAuthorizeException.class)
-    public ResponseEntity<ErrorResponse> handleNotAuthorizaException() {
-        return new ResponseEntity<>(ErrorResponse.of(NOT_AUTHORIZED_CODE,NOT_AUTHORIZED_MESSAGE), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(MemberExistException.class)
-    public ResponseEntity<ErrorResponse> handleMemberExistException() {
-        return new ResponseEntity<>(ErrorResponse.of(MEMBER_EXISTS_CODE,MEMBER_EXISTS_MESSAGE), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(NotFoundCertificationException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundCertificationException() {
-        return new ResponseEntity<>(ErrorResponse.of(NOT_FOUND_CERTIFICATION_CODE,NOT_FOUND_CERTIFICATION_MESSAGE), HttpStatus.BAD_REQUEST);
-    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException() {
         return new ResponseEntity<>(ErrorResponse.of(VALIDATION_FAILED_CODE,VALIDATION_FAILED_MESSAGE), HttpStatus.BAD_REQUEST);
