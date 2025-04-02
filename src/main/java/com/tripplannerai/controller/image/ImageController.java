b385e5079db1f9ed1a5ef0ee649c124439a6805f
preej <preejer8013@gmail.com>
2025-04-02 18:01:37 +0900

    image controller

--- src/main/java/com/tripplannerai/controller/image/ImageController.java
+++ src/main/java/com/tripplannerai/controller/image/ImageController.java
@@ -1,21 +1,23 @@
 package com.tripplannerai.controller.image;
 
-import com.tripplannerai.annotation.Username;
 import com.tripplannerai.service.image.ImageService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
     @GetMapping("/image/{id}")
-    public Resource fetchImage(@PathVariable Long id, @Username String email) throws IOException {
-        return imageService.fetchImage(id);
+    public Resource fetchImage(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        return imageService.fetchImage(id,response);
     }
 }
