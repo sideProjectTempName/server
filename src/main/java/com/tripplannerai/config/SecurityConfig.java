1c8a877d489f1f41438c4bec755a9cbf369e9012
qwerrewq123 <preejer8013@gmail.com>
2025-09-05 19:53:35 +0900

    delete cors

--- src/main/java/com/tripplannerai/config/SecurityConfig.java
+++ src/main/java/com/tripplannerai/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Bean
+//    @Bean
     protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedHeader("*");
