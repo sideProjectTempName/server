ef9553d8e398b77663c07ce11672478199f47f67
qwerrewq123 <preejer8013@gmail.com>
2025-09-06 21:56:09 +0900

    commit

--- src/main/java/com/tripplannerai/repository/member/MemberRepository.java
+++ src/main/java/com/tripplannerai/repository/member/MemberRepository.java
@@ -13,7 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     @Query("select m from Member m left join fetch m.image i where m.id = :memberId")
     Optional<Member> fetchById(Long memberId);
-    @Query("update Member m set m.ticket = m.ticket - :val where m.id = :userId")
+    @Query("update Member m set m.ticket = m.ticket + :val where m.id = :userId")
     @Modifying
     void updateTicket(Integer val, Long userId);
 
--- src/main/java/com/tripplannerai/service/payment/PaymentService.java
+++ src/main/java/com/tripplannerai/service/payment/PaymentService.java
@@ -71,7 +71,7 @@ public class PaymentService {
             ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
             if (response.getStatusCode() == HttpStatus.OK) {
                 paymentRepository.save(PaymentFactory.from(paymentRequest, member));
-                memberRepository.updateTicket(paymentRequest.getAmount(), id);
+                memberRepository.updateTicket(paymentRequest.getAmount() / 200, id);
             }
         }catch (Exception e){
             log.error(e.getMessage(),e);
