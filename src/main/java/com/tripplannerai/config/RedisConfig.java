aa429893312c668dc62ba9daa1658c0b87a69da6
qwerrewq123 <preejer8013@gmail.com>
2025-09-06 20:46:08 +0900

    commit

--- src/main/java/com/tripplannerai/config/RedisConfig.java
+++ src/main/java/com/tripplannerai/config/RedisConfig.java
@@ -12,6 +12,7 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory connectionFactory){
+
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
