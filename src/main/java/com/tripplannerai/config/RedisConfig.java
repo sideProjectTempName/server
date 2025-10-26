088383dd1f7bb6d8672cd53ba35b705d30d6e9ce
dongkyukim1 <wlsntus55@gmail.com>
2025-10-26 22:10:54 +0900

    fix: add @Primary annotation to RedisTemplate bean to resolve dependency injection conflict

--- src/main/java/com/tripplannerai/config/RedisConfig.java
+++ src/main/java/com/tripplannerai/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.tripplannerai.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -11,6 +12,7 @@ public class RedisConfig {
 
 
     @Bean
+    @Primary
     public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory connectionFactory){
 
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
